4b1b52a0651156b9c354fc9014f5ec26233282bb
Arnaud Ferrand <arferrand@gmail.com>
2021-03-25 13:09:19 +0100

    improves review page with shotcut to edit

--- src/components/ReviewRegistration.js
+++ src/components/ReviewRegistration.js
@@ -3,6 +3,20 @@ import React, { useContext } from 'react';
 
 import { Form, Formik } from 'formik';
 import {
+  Avatar,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemSecondaryAction,
+  ListItemText,
+} from '@material-ui/core';
+import BusinessIcon from '@material-ui/icons/Business';
+import EditIcon from '@material-ui/icons/Edit';
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
+import PersonIcon from '@material-ui/icons/Person';
+
+import {
   DispatchContext,
   RegistrationContext,
 } from 'contexts/register.context';
@@ -27,17 +41,102 @@ const ReviewRegistration = ({
         type: REGISTER,
         payload: values,
       });
+      // eslint-disable-next-line no-alert
+      alert(JSON.stringify(values, null, 2));
       setSubmitting(false);
       //
       setActiveStep(activeStep + 1);
     }, 500);
   };
 
+  const { company, accounting, shareholders } = registration;
+
   return (
     <Formik initialValues={registration} onSubmit={handleSubmit}>
       {({ isSubmitting, submitForm }) => (
         <React.Fragment>
           <Form>
+            {/* COMPANY */}
+            <List>
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <BusinessIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary={company.name}
+                  secondary={`${company.address1} ${company.address2}- ${company.zipcode} ${company.city}`}
+                />
+                <ListItemSecondaryAction>
+                  <IconButton
+                    edge="end"
+                    aria-label="edit"
+                    onClick={() => {
+                      // TODO next is review
+                      setActiveStep(0);
+                    }}
+                  >
+                    <EditIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            </List>
+            {/* SHAREHOLDERS */}
+            <List>
+              {shareholders.map((shareholder, idx) => (
+                <ListItem key={idx}>
+                  <ListItemAvatar>
+                    <Avatar>
+                      <PersonIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={`${shareholder.firstName} ${shareholder.lastName}`}
+                    secondary={`${shareholder.birthdate} - ${shareholder.email}`}
+                  />
+                  <ListItemSecondaryAction>
+                    <IconButton
+                      edge="end"
+                      aria-label="edit"
+                      onClick={() => {
+                        // TODO: next => review
+                        setActiveStep(1);
+                      }}
+                    >
+                      <EditIcon />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              ))}
+            </List>
+            {/* ACCOUNTING */}
+            <List>
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <AccountBalanceIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary={`Company ${accounting.vat ? 'with' : 'without'} VAT`}
+                  secondary={`${
+                    accounting.vat ? `VAT Number ${accounting.vatnumber}` : ''
+                  }`}
+                />
+                <ListItemSecondaryAction>
+                  <IconButton
+                    edge="end"
+                    aria-label="edit"
+                    onClick={() => {
+                      setActiveStep(2);
+                    }}
+                  >
+                    <EditIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            </List>
             <pre>{JSON.stringify(registration, null, 2)}</pre>{' '}
             <ActionButtons
               activeStep={activeStep}
